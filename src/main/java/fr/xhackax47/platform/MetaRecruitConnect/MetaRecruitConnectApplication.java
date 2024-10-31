b9571dce07958047cdef2b800ba74c0b0bd9da42
xhackax47 <xhackax47@gmail.com>
2024-10-31 09:20:38 +0100

    [0.0.1] Initialisation Spring et dépendances

---
+++ src/main/java/fr/xhackax47/platform/MetaRecruitConnect/MetaRecruitConnectApplication.java
@@ -0,0 +1,13 @@
+package fr.xhackax47.platform.MetaRecruitConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaRecruitConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaRecruitConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/xhackax47/platform/MetaRecruitConnect/MetaRecruitConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.xhackax47.platform.MetaRecruitConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaRecruitConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
